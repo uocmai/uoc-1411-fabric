73d0b1ebfe8dae86dd51d5b63b5550eb6de32b3f
maiuoc <maiuoc.k8c@gmail.com>
2015-08-13 16:42:47 +0700

    mai uoc commit ve nha lam

--- path2/gradients.php
+++ path2/gradients.php
@@ -2,48 +2,59 @@
 <head>
     <meta charset="utf-8">
     <title>fabric.js-simple text display</title>
-	
-	<!-- Get version 1.1.0 of Fabric.js from CDN -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/fabric.js/1.1.0/fabric.all.min.js" ></script>
- 
-	<!-- Get the highest 1.X version of jQuery from CDN. Required for ready() function. -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script> 
+	<script src="js/fabric.js" ></script>
  </head>
-
 <body>
 	<!-- A canvas tag is required or Fabric.js doesn't know where to draw. -->
 	<canvas id="c" style="border:1px solid black;" ></canvas>
-		<script>
-	$(function () {
-		var canvas = new fabric.Canvas('c');
-		/* canvas.setWidth(500);
-		canvas.setHeight(350); */
-		var hi = new fabric.Text('hello, world.', {
+	<img src="../images/cat.jpg" id="my-image" style="display: none">
+	<img src="../images/Dau-Tay_4.jpg" id="my-dautay" style="display: none">
+	<script>
+	var canvas = new fabric.Canvas('c');
+		canvas.setWidth(500);
+		canvas.setHeight(350);
+		var imgElement = document.getElementById('my-image');
+		var imgInstance = new fabric.Image(imgElement, {
+		  left: 100,
+		  top: 100,
+		 //  angle: 30,
+		  opacity: 0.85
+		});
+		var dautayElement = document.getElementById('my-dautay');
+		var dautay = new fabric.Image(dautayElement, {
+		  left: 100,
+		  top: 100,
+		 //  angle: 30,
+		  opacity: 0.85
+		});
+		 var hi = new fabric.Text('Hello Pé Dâu Tây', {
 			left: canvas.getWidth() / 2,
 			top: canvas.getHeight() / 2,
 			fill: 'red',			
 		});
 		var circle = new fabric.Circle({
-			left: 100,
-			top: 100,
-			radius: 50
+		  left: 100,
+		  top: 100,
+		  radius: 50
 		});
-		/* circle.setGradient('fill', {
+		circle.setGradient('fill', {
 		  x1: -circle.width / 2,
 		  y1: 0,
 		  x2: circle.width / 2,
 		  y2: 0,
 		  colorStops: {
 			0: "red",
-			1: "blue"
+			0.2: "orange",
+			0.4: "yellow",
+			0.6: "green",
+			0.8: "blue",
+			1: "purple"
 		  }
-		}); */
-		canvas.add(circle);
-		//canvas.add(hi);
-		
-	});
+		});
+		canvas.add(imgInstance);
+		canvas.add(dautay);
+		canvas.add(hi);
+		// canvas.add(circle);
 	</script>
 </body>
-
-
 </html>
